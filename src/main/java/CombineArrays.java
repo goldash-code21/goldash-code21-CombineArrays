5b45c6cf4f9e13851564d95813b6507556be7276
ashley quijada <ashqui@protonmail.com>
2025-03-01 00:02:04 +0000

    completed combineArrays

--- src/main/java/CombineArrays.java
+++ src/main/java/CombineArrays.java
@@ -7,8 +7,13 @@ public class CombineArrays {
      * @param arr2 an array of ints.
      * @return an array containing the contents of arr1 followed by the contents of arr2.
      */
-    public int[] combine(int[] arr1, int[] arr2){
+    public int[] combine(int[] arr1, int[] arr2) {
+        int len1 = arr1.length;
+        int len2 = arr2.length;
+        int[] arr3 = new int[len1+len2];
 
-        return null;
+        System.arraycopy(arr1, 0, arr3, 0, len1);
+        System.arraycopy(arr2, 0, arr3, len1, len2);
+        return arr3;
     }
 }
